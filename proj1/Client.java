e9219fe74fc48e7e7094502448d1895231aadad2
orezahc <chazerod@gmail.com>
2016-01-13 10:35:54 -0800

    initialized.

---
+++ proj1/Client.java
@@ -0,0 +1,52 @@
+import java.net.*;
+import java.io.*;
+
+public class Client {
+	public static void main(String [] args)
+	{
+		if(args.length < 2)
+		{
+			System.out.print("Usage: java TCPClient <Host Name/IP> <Port Number> [Command]\n"
+					+ "\tCommand: set <key> <value>\n"
+					+ "\t         get <key>\n"
+					+ "\t         del <key>\n");
+			System.exit(1);
+		}
+		
+		String serverName = args[0];
+		int port = Integer.parseInt(args[1]);
+		String packet="";
+		try
+		{
+			if(args.length>2)
+			{
+				packet = args[2];
+				
+			}
+			if(args.length>3)
+			{
+				packet = packet + " " + Integer.parseInt(args[3]);
+			}
+			if(args.length>4)
+			{
+				packet = packet + " " + args[4];
+			}
+			System.out.println(packet);
+			System.out.println("Connecting to " + serverName + " on port " + port);
+			Socket client = new Socket(serverName, port);
+			System.out.println("Just connected to " + client.getRemoteSocketAddress());
+			OutputStream outToServer = client.getOutputStream();
+			DataOutputStream out = new DataOutputStream(outToServer);
+//			out.writeUTF("Hello from " + client.getLocalSocketAddress());
+			out.writeUTF(packet);
+			InputStream inFromServer = client.getInputStream();
+			DataInputStream in = new DataInputStream(inFromServer);
+			System.out.println("Server says " + in.readUTF());
+			client.close();
+			
+		}catch(IOException e)
+	    {
+			e.printStackTrace();
+	    }
+	}
+}
---
+++ proj1/TCPServer.java
@@ -0,0 +1,89 @@
+import java.net.*;
+import java.io.*;
+
+
+public class TCPServer extends Thread{
+	private ServerSocket serverSocket;
+	
+	public TCPServer(int port) throws IOException
+	{
+	      serverSocket = new ServerSocket(port);
+	      serverSocket.setSoTimeout(30000);
+	}
+	
+	public void set()
+	{
+		
+	}
+	
+	public void get()
+	{
+		
+	}
+	
+	public void del()
+	{
+		
+	}
+	
+	public void terminate()
+	{
+		
+	}
+	
+	public void requestHandler()
+	{
+		
+	}
+	
+	
+	public void run()
+	{
+		while(true)
+		{
+			try
+			{
+				System.out.println("Waiting for client on port " +
+					serverSocket.getLocalPort() + "...");
+				Socket server = serverSocket.accept();
+					System.out.println("Just connected to " + server.getRemoteSocketAddress());
+				DataInputStream in =
+					new DataInputStream(server.getInputStream());
+				System.out.println(in.readUTF());
+				DataOutputStream out =
+				    new DataOutputStream(server.getOutputStream());
+					out.writeUTF("Thank you for connecting to "
+					  + server.getLocalSocketAddress() + "\nGoodbye!");
+				    server.close();
+			}catch(SocketTimeoutException s)
+			{
+			    System.out.println("Socket timed out!");
+			    break;
+			}catch(IOException e)
+			{
+			    e.printStackTrace();
+			    break;
+			}
+		}
+	}
+	
+	public static void main(String [] args)
+	{
+		if(args.length < 1) 
+		{
+			System.out.println("Usage: java TCPServer <Port Number>");
+			System.exit(1);
+		}
+		
+		int port = Integer.parseInt(args[0]);
+		
+		try
+		{
+			Thread t = new TCPServer(port);
+			t.start();
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
