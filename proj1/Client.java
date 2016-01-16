c7421231ff16ad9569fc501d5e837fb93e438bc8
Anam <nmzahid92@gmail.com>
2016-01-15 19:41:27 -0800

    Added UDP
    Added UDP and modified Server and Client to invoke UDP server and
    client.

--- proj1/Client.java
+++ proj1/Client.java
@@ -8,24 +8,38 @@ public class Client {
 		String serverName = args[0];
 		int port = Integer.parseInt(args[1]);
 		
+                String UDPServerName = args[3];
+		int UDPport = Integer.parseInt(args[4]);
 		TCPClient client = new TCPClient(serverName, port);
+                UDPClient client2=new UDPClient(UDPServerName,UDPport);
 
 		while(true){
 			System.out.print(">");
 			String input = System.console().readLine();
 			Log.log(input);
 			if(client.connect() == false){
-				Log.log("Connecting to " + serverName + " on port " + port + " failed!");
+				Log.log("Connecting to TCP Server at " + serverName + " on port " + port + " failed!");
 				continue;
 			}
+                        else if(client2.connect()==false)
+                        {
+                            Log.log("Connecting to UDP Server at " + serverName + " on port " + port + " failed!");
+				continue;
+                        }
 			else{
-				Log.log("Connecting to " + serverName + " on port " + port);
-				Log.log("Just connected to " + client.getRemoteSocketAddress());
-			}
+				Log.log("Connecting to TCP Server at " + serverName + " on port " + port);
+				Log.log("Just connected to TCP Server at " + client.client.getRemoteSocketAddress()+"\n------------------\n");
+			        Log.log("Connecting to UDP Server at " + UDPServerName + " on port " + UDPport);
+				Log.log("Just connected to UDP Server at " + serverName+":"+port+"\n------------------\n");
+			
+                        }
 
 			String response = client.sendRequest(input.split(" "));
+                        String response2 = client2.sendRequest(input.split(" "));
 			client.disconnect();
+                        client2.disconnect();
 			Log.log(response);
+                        Log.log(response2);
 		}
 	}
 }
--- proj1/Server.java
+++ proj1/Server.java
@@ -1,3 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author anamzahid
+ */
 import java.net.*;
 import java.io.*;
 
@@ -7,16 +17,19 @@ public class Server extends Thread{
 	{
 		if(args.length < 1) 
 		{
-			System.out.println("Usage: java TCPServer <Port Number>");
+			System.out.println("Usage: java TCPServer <Port Number> UDPServer <Port Number>");
 			System.exit(1);
 		}
-		
-		int port = Integer.parseInt(args[0]);
-		
+		System.out.println(args[0]+" "+args[1]+" "+args[2]);
+		int tcpport = Integer.parseInt(args[0]);
+                
+		int udpport=Integer.parseInt(args[2]);
 		try
 		{
-			Thread t = new TCPServer(port);
+			Thread t = new TCPServer(tcpport);
 			t.start();
+                        Thread t2=new UDPServer(udpport);
+                        t2.start();
 		}catch(IOException e)
 		{
 			e.printStackTrace();
