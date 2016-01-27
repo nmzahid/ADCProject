d8ff77985634d10683b32c345d459555a127b679
aloon0618 <aloon0618@gmail.com>
2016-01-27 15:58:15 -0800

    Update Log.py

--- proj2/Log.py
+++ proj2/Log.py
@@ -1,5 +1,10 @@
-class Log:
-	def __init__(self, filename):
+import datetime
 
-	def log(slef, logStr):
-		
+class Log:
+	def __init__(self, fileName):
+		self.fileName = fileName
+
+	def log(self, data):
+		logStr = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		with open(self.fileName, "a") as myfile:
+			myfile.write("["+ logStr + "] " + data + "\n")
